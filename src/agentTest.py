ceb71940bf604906faf241818716f03be7948ef2
Msouza <msdsantos96@gmail.com>
2022-07-06 23:13:53 -0300

    mostrar score de agente treinado

--- src/agentTest.py
+++ src/agentTest.py
@@ -6,22 +6,30 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_atari_env
 import os
 
-a2c_path = 'D:\Projetos\AgentBreakout\modelos.zip'
+a2c_path = 'D:\Projetos\AgentBreakout\modelos\AgenteTreinado.zip'
 
+
+               
 env = make_atari_env('Breakout-v0', n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4)
-agent = A2C.load(a2c_path, env)
 
+model = A2C.load(a2c_path, env)
 
-evaluate_policy(agent, env, n_eval_episodes=10, render=True)
-obs = env.reset()
-score = 0
-while True:
-    action, _states = agent.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    episode = agent._episode_num
-    score+= rewards
-    env.render()
+evaluate_policy(model, env, n_eval_episodes=10, render=True)
+
+episodes =50
+
+for episode in range(1, episodes+1):
+    obs = env.reset()
+    #state = env.reset()
+    done = False
+    score = 5
+    
+    while not done:
+        #env.render()
+        action,_ = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        score+=reward
     print('Episode:{} Score:{}'.format(episode, score))
     
 env.close()
